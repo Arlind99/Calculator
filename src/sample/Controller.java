0b60fb3f270bec98077b10a2dc75055b112143ea
Arlind99 <arlindpodrimqaku25@gmail.com>
2020-01-16 14:03:03 +0100

    Pjesa e pare e kalkulatorit

---
+++ src/sample/Controller.java
@@ -0,0 +1,12 @@
+package sample;
+
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.control.TextField;
+
+public class Controller {
+    @FXML
+    Button btn1;
+    @FXML
+    TextField txtF1;
+}
